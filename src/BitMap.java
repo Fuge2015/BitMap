955f5fc053d433c59a5d85c6360c99ed198c8af3
Fuge2015 <995253883@qq.com>
2017-09-27 10:26:38 +0800

    add a constant array named BITMASK to reduce calculation while setting, getting,clearing.

--- src/BitMap.java
+++ src/BitMap.java
@@ -2,6 +2,8 @@
  * Created by dongfu on 17-9-23.
  */
 public class BitMap {
+
+    private final int[] BITMASK = new int[32];
     private final int[] map;
     private final int size;
 
@@ -18,15 +20,23 @@ public class BitMap {
 //            }
             size = n;
         }
+
+        int count = 0;
+        while(count < 32) {
+            BITMASK[count] = 0x8000_000 >>> count;
+            count++;
+        }
     }
+
+
 
     public void set(int n) {
         if (n < size) {
             int count = n >> 5;
             int s = n << 27 >>> 27;
-            int temp = 0x8000_0000;
-            temp = temp >>> s;
-            map[count] |= temp;
+//            int temp = 0x8000_0000;
+//            temp = temp >>> s;
+            map[count] |= BITMASK[s];
         } else {
             throw new IndexOutOfBoundsException();
         }
@@ -36,10 +46,10 @@ public class BitMap {
         if (n < size) {
             int count = n >> 5;
             int s = n << 27 >>> 27;
-            int temp = 0x8000_0000;
-            temp = temp >>> s;
-            temp = ~temp;
-            map[count] = map[count] & temp;
+//            int temp = 0x8000_0000;
+//            temp = temp >>> s;
+//            temp = ~temp;
+            map[count] = map[count] & ~BITMASK[s];
         } else {
             throw new IndexOutOfBoundsException();
         }
@@ -49,9 +59,9 @@ public class BitMap {
         if (n < size) {
             int count = n >> 5; //  求得count = n/5的商,即map中的第count个int元素.
             int s = n << 27 >>> 27; // 求得s = n/5的余数,即第count个int元素的第s位.
-            int temp = 0x8000_0000;
-            temp = temp >>> s;
-            return (map[count] & temp) != 0 ? 1 : 0; // 此处使用"!=",而不是">"的原因是map[count]如果为0x8000_0000时,依然返回1.
+//            int temp = 0x8000_0000;
+//            temp = temp >>> s;
+            return (map[count] & BITMASK[s]) != 0 ? 1 : 0; // 此处使用"!=",而不是">"的原因是map[count]如果为0x8000_0000时,依然返回1.
         } else {
             throw new IndexOutOfBoundsException();
         }
